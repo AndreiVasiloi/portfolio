9dc9817fffea3bdd83d96d701d21e21b9d9a3b91
Andrei Vasiloi <andrei_vasiloi@yahoo.com>
2021-03-13 19:49:17 +0200

    added navigation

--- src/features/navigation/NavBar.jsx
+++ src/features/navigation/NavBar.jsx
@@ -1,17 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../css/_navigation.scss";
-import HamburgerIcon from "./HamburgerIcon";
 
 export default function NavBar() {
+  let [showNav, setShowNav] = useState(true);
   return (
     <>
-      <ul className="nav">
-        <HamburgerIcon />
-        <li>Work</li>
-        <li>Biography</li>
-        <li>Experience</li>
-        <li>Contact</li>
-      </ul>
+      <div className="navigation">
+        <div
+          className="navigation__button"
+          onClick={() => setShowNav(!showNav)}
+        >
+          <span
+            className={
+              showNav
+                ? "navigation__icon"
+                : "navigation__icon navigation__icon-toggle"
+            }
+          >
+            &nbsp;
+          </span>
+        </div>
+        <div
+          className={
+            showNav
+              ? "navigation__background"
+              : "navigation__background navigation__toggle"
+          }
+        >
+          &nbsp;
+        </div>
+        <nav
+          className={
+            showNav
+              ? "navigation__nav"
+              : "navigation__nav navigation__nav-active"
+          }
+        >
+          <ul className="navigation__list">
+            <li className="navigation__item">
+              <a href="link" className="navigation__link">
+                Work
+              </a>
+            </li>
+            <li className="navigation__item">
+              <a href="link" className="navigation__link">
+                Biography
+              </a>
+            </li>
+            <li className="navigation__item">
+              <a href="link" className="navigation__link">
+                Experience
+              </a>
+            </li>
+            <li className="navigation__item">
+              <a href="link" className="navigation__link">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </>
   );
 }
