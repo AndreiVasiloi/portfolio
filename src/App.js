b056eb5305b51a5c96ea87ef3f1be9010e842263
Andrei <andrei_vasiloi@yahoo.com>
2021-03-14 20:54:49 +0200

    added section home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./css/_base.scss";
+import HomePage from "./features/home/HomePage";
 import NavBar from "./features/navigation/NavBar";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <HomePage/>
     </div>
   );
 }
---
+++ src/features/home/HomePage.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "../../css/_home.scss";
+
+export default function HomePage() {
+  return (
+    <section className="section__home">
+      <h1 className="home__header">
+        <div className="home__header-fr">
+          <span className="home__header-char home__header-char-fr">H</span>
+          <span className="home__header-char home__header-char-fr">i</span>
+          <span className="home__header-char home__header-char-fr">,</span>
+        </div>
+        <div className="home__header-sr">
+          <span className="home__header-char home__header-char-sr">I</span>
+          <span className="home__header-char home__header-char-sr">`</span>
+          <span className="home__header-char home__header-char-sr">
+            m&nbsp;
+          </span>
+          <span className="home__header-char home__header-char-sr">A</span>
+          <span className="home__header-char home__header-char-sr">n</span>
+          <span className="home__header-char home__header-char-sr">d</span>
+          <span className="home__header-char home__header-char-sr">r</span>
+          <span className="home__header-char home__header-char-sr">e</span>
+          <span className="home__header-char home__header-char-sr">i</span>
+          <span className="home__header-char home__header-char-sr">,</span>
+        </div>
+      </h1>
+      <h3 className="home__header-3">a self-taught web developer</h3>
+      <a href="#projects" className="home__btn">
+        my projects
+      </a>
+    </section>
+  );
+}
